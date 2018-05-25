a607c2de1080a6f7f878f61ae5948b8e813f2b5c
EC2 Default User <ec2-user@ip-172-31-38-111.us-east-2.compute.internal>
2018-05-25 09:42:10 +0000

    CRUD tasklist repair

--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -7,5 +7,9 @@
     <p>{{ $tasklist->content }}</p>
     
     {!! link_to_route('tasklists.edit','このタスクを編集',['id' => $tasklist->id])  !!}
+    
+    {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id],'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
 
 @endsection
