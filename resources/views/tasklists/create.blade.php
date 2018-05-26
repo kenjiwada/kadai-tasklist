50393eea2bb6d6e28b535ead1658c0751332573a
EC2 Default User <ec2-user@ip-172-31-38-111.us-east-2.compute.internal>
2018-05-26 08:00:11 +0000

    tasklist repair

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -5,6 +5,9 @@
     <h1>タスク追加ページ</h1>
 
      {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
+
+         {!! Form::label('status','ステータス:') !!}
+         {!! Form::text('status') !!}
          
          {!! Form::label('content','タスク:') !!}
          {!! Form::text('content') !!}
